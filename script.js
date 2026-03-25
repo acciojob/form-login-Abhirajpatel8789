5de4f8cee34db35bb5e9720cd0148ed871fa2a1d
Abhirajpatel8789 <122907724+Abhirajpatel8789@users.noreply.github.com>
2026-03-25 22:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+let fname = document.querySelector("#fname");
+let lname = document.querySelector("#lname")
+
 function getFormvalue() {
     //Write your code here
+	alert(`${fname.value} ${lname.value}`)
 
 }
